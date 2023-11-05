7f3043e65beb2a1b482ef0f084003379a4027128
hasuto <96404939+hasuto@users.noreply.github.com>
2023-11-05 10:31:24 +0900

    マイグレーションを行い、mysqlと連携完了しました。ユーチューブのロゴの追加。ナビゲーションバーのリンクの実装を完了しました。

--- laravel/resources/js/Pages/Welcome.vue
+++ laravel/resources/js/Pages/Welcome.vue
@@ -2,31 +2,22 @@
 import { Head, Link } from '@inertiajs/vue3';
 import NavLayout from '@/Layouts/NavLayout.vue'
 import VideoCard from '@/Components/VideoCard.vue';
-// defineProps({
-//     canLogin: {
-//         type: Boolean,
-//     },
-//     canRegister: {
-//         type: Boolean,
-//     },
-//     laravelVersion: {
-//         type: String,
-//         required: true,
-//     },
-//     phpVersion: {
-//         type: String,
-//         required: true,
-//     },
-// });
+defineProps({
+    videos: Array
+});
 </script>
 
 <template>
     <Head title="Youtube" />
     <NavLayout>
         <div class="grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2">
-            <VideoCard title="Cool city views" user="John weeks Dev" views="23k views -3 days ago"
-                image="https://picsum.photos/id/230/100" videoUrl="/videos/seoul.mp4"
-                thumbnail="/videos/Thumbnails/seoul.png" />
+
+            <div v-for="video, index in videos" :key="video">
+                <Link :href="route('videos.show', { id: video.id })">
+                <VideoCard :title="video.title" :user="video.user" :views="video.views"
+                    :image="`https://picsum.photos/id/${index}/100`" :videoUrl="video.video" :thumbnail="video.thumbnail" />
+                </Link>
+            </div>
         </div>
     </NavLayout>
 </template>
